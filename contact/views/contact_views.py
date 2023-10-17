801d43d64a7c227d5d683fcb860a0378c2866d58
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-10-17 15:45:48 -0300

    adicionado paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
@@ -6,11 +7,14 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects \
         .filter(show=True) \
-        .order_by('-id')[:10]
+        .order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Contatos -'
+        'page_obj': page_obj,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -36,9 +40,13 @@ def search(request):
             )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Search -'
+        'page_obj': page_obj,
+        'site_title': 'Search - '
     }
 
     return render(
