d07114ed56742744aa761946aa9290ad4b03999b
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-10-18 11:01:08 -0300

    adicionado forms base, adicionado validor de campo teste, mas forms ainda em testes

---
+++ contact/forms.py
@@ -0,0 +1,72 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Escreva aqui:'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para o usuário',
+    )
+
+    def __ini__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'placeholder': 'Escreva aqui:'
+        # })
+
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone'
+            )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b'
+        #         }
+        #     )
+        # }
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primero nome não pode ser igual ao segundo',
+                code='invalid',
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Mensagem de erro',
+                    code='invalid'
+                )
+            )
+
+        return first_name
